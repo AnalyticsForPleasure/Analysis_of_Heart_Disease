b226b1a1cbba3a5f247dbb0202f368760b48f0e3
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-03-20 21:37:59 +0200

    Starting to create the data for horazontal bar chart with the diff %

--- Questions/Question_1/Question_1.py
+++ Questions/Question_1/Question_1.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from pprint import pprint
 
-
 ######### info about the data ############################################################################################################################################################
 # HeartDisease - target trait.
 # BMI - a value that allows you to assess the degree of correspondence between a person's mass and his height, and thereby indirectly judge whether the mass is insufficient, normal or excessive. It is important in determining the indications for the need for treatment.
